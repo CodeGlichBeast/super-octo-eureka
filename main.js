b5cd4be4d486d8722e6f6d4106a9b771deb280f2
CodeGlichBeast <78890570+CodeGlichBeast@users.noreply.github.com>
2021-10-07 20:47:49 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -9,22 +9,39 @@ Recognition.onresult=function(event){
     console.log(event);
     var content=event.results[0][0].transcript;
     document.getElementById("textbox").innerHTML=content;
+    console.log(content);
+    if(content == "take my selfie"){
+        speech();
+        console.log("taking selfie")
+    }
+    
 
-    speech();
+    
 }
 
 function speech(){
     var synth = window.speechSynthesis;
-    speech_data = document.getElementById("textbox").value + ", Will take selfie in 10 seconds";
+    speech_data = "Will take selfie in 10 seconds";
     var talk= new SpeechSynthesisUtterance(speech_data);
     synth.speak(talk);
 
-    webcam.attach(camera);
+    Webcam.attach(camera);
 
-
+    setTimeout(function(){
+        take_snapshot();
+        save();
+    }, 10000);
+    
 }
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("display").innerHTML='<img id="my_selfie" src="'+data_uri+'" />';
+    });
+    
+}
+
 camera=document.getElementById("camera")
-webcam.set({
+Webcam.set({
 
     width:360,
     height:400,
@@ -32,3 +49,9 @@ webcam.set({
     jpeg_quality:100,
 
 });
+function save(){
+    link=document.getElementById("link");
+    image=document.getElementById("my_selfie").src;
+    link.href = image;
+    link.click();
+}
